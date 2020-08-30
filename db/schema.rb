3da7a9106e9df5eb11e9c8afc16d80e81451134b
Wynterwolf <shinyguns@gmail.com>
2020-08-30 17:59:54 -0500

    schema update and show.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200830190214) do
+ActiveRecord::Schema.define(version: 20200830223827) do
+
+  create_table "dogs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "breed"
+    t.string   "age"
+    t.integer  "foster"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
