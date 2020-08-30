55ad6226445fbb86439f75ee36ad9e4198cfe067
Wynterwolf <shinyguns@gmail.com>
2020-08-30 16:57:28 -0500

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
 
-    get '/login' do
-        erb :'/sessions/login'
+    get '/sessions/login' do
+        erb :'sessions/login'
     end
 
     post '/login' do
         # find the user by their email:
-        user = User.find_by_email(params[:email])
+        @user = User.find_by_email(params[:email])
         # if they typed in the right password then log them in, if not show them the form again
-        if user && user.authenticate(params[:password]) 
-          session[:id] = user.id
+        if @user && @user.authenticate(params[:password]) 
+          session[:id] = @user.id
           redirect "/"
         else 
           @error = "Incorrect email or password"
