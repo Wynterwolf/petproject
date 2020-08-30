d884714c3ddeb9ed0a69a1e9b4bf20c22be56a96
Wynterwolf <shinyguns@gmail.com>
2020-08-30 14:01:06 -0500

    add sessions and secret to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # use sessions and session secret
+    set :session, true
+    set :sessions_secret, ENV["SESSION_SECRET"]
   end
 
   get "/" do
