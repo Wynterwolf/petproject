b84817cfd81e9d5e7d5a601185410725efd985b3
Wynterwolf <shinyguns@gmail.com>
2020-08-30 13:56:45 -0500

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+ENV['SINATRA_ENV'] ||= "development"
+
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+# gem for sessions/cookie/password
+Dotenv.load
+
+require './app/controllers/application_controller'
+require_all 'app'
