1a423170acc844619788e16feae75c39cce04ad0
Wynterwolf <shinyguns@gmail.com>
2020-08-30 17:55:33 -0500

    add dog table and migrate

---
+++ app/models/dog.rb
@@ -0,0 +1,6 @@
+class Dog < ActiveRecord::Base
+    belongs_to :foster, class_name: "User"
+    validates :name, presence: true
+    validates :breed, presence: true
+    validates :age, presence: true
+end
