94b21f068633b62fdb728b0987eeb880c5560218
Wynterwolf <shinyguns@gmail.com>
2020-08-30 16:12:02 -0500

    login doesn't work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
     get '/login' do
-        erb :'/sessions/login'
+        erb :'sessions/login'
     end
 
     post '/login' do 
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
           redirect "/"
         else 
           @error = "Incorrect email or password"
-          erb :'/sessions/login'
+          erb :'sessions/login'
         end
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
 
     get '/users/new' do
         #get form for account creation
-        erb :'/users/new'
+        erb :'users/new'
     end
 
     post '/users' do
         @user = User.new(email: params[:email], password: params[:password])
         if @user.save
-            sessions[:id] = @user.id
+            session[:id] = @user.id
             redirect "/"
         else
             erb :'users/new'
