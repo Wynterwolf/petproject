a7519fdd1a7e25277f138d33e9e538326dd5baf6
Samantha Jordan <shinyguns@gmail.com>
2020-08-31 10:49:01 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    
+    set :public_folder, File.dirname(FILE) + '/static'
   end
 
   get "/" do
