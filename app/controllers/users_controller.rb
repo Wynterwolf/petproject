9784d79ed6231d89c4153c68622e848df0cdf2b3
Wynterwolf <shinyguns@gmail.com>
2020-09-15 18:53:08 -0500

    added edit/show erb & get/patch user email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,12 +19,55 @@ class UsersController < ApplicationController
 
     get '/users/:id' do
         #Work on the show page to display user information
-    
+        @user = User.find_by_id(params[:id])
+            if @user.nil?
+                flash[:error] = "No user by that name"
+                redirect "/user/show.html"
+            end
         #greet user
-
+            @dogs = @user.dogs
         #list their owned dogs
 
         #edit button for changing username
-        erb: 
+        erb :'/users/show.html'
+    end
+
+    get "/users/:id/edit" do
+        set_user
+        redirect_if_not_authorized
+        erb :"/users/edit.html"
+    end
+
+    patch "/users/:id" do
+        set_user
+        redirect_if_not_authorized
+        if @user.update(email: params[:user][:email])
+           flash[:success] = "User successfully updated"
+          redirect "/users/#{@user.id}"
+        else 
+          erb :"/users/edit.html"
+        end
+      end
+    
+    private
+    
+    def set_user
+        @user = User.find_by_id(params[:id])
+        if @user.nil?
+            flash[:error] - "No user found"
+            redirect "/"
+        end
+    end
+
+    def redirect_if_not_authorized
+        redirect_if_not_logged_in
+        if !authorize_user(@user)
+            flash[:error] = "No permission"
+            redirect "/"
+        end
+    end
+
+    def authorize_user(user)
+        current_user == user
     end
 end
